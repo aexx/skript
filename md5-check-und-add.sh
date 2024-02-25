c2ea5b8ff003f129eb1aee449d40e8d46a005990
aexx <alex.schilling@gmx.net>
2024-02-25 11:14:23 +0100

    2024-02-25_11:14:23 aspiX3950

---
+++ md5-check-und-add.sh
@@ -0,0 +1,21 @@
+#
+if [ $# -lt 1 ]
+  then
+  echo "EXIT - NO md5-file"
+  echo "$0 meine-md5-datei.md5"
+  exit 1
+fi
+
+[ -f $1 ] || echo "EXIT - NO existing md5-file"
+[ -f $1 ] || exit 1
+
+ls *|grep -v ".md5" |while read f
+do 
+  #sleep 1
+  [ -f "$f" ] && grep -q $f $1 && ls -la $f 
+  [ -f "$f" ] && ( grep -q $f $1 || echo "FILE:  $f  NOT in  $1  => ADD " )
+  [ -f "$f" ] && ( grep -q $f $1 || md5sum $f >> $1 )
+done
+echo "
+CHECK:  md5sum -c $1
+"
