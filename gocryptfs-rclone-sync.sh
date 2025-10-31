8004be2f6ccaa964da9307ea352a653a73b8a21b
aexx <aexx@users.noreply.github.com>
2025-10-31 16:50:31 +0100

    Update gocryptfs-rclone-sync.sh
    Design

--- gocryptfs-rclone-sync.sh
+++ gocryptfs-rclone-sync.sh
@@ -58,7 +58,7 @@ printf "${g}
 │ rclone-Remote-Path: ${n} $4 ${g} $(tput hpa 71) │
 ╰───────────────────────────────────────────────────────────────────────╯ "
 printf "${g} $(tput hpa 55)┬\n$(tput hpa 55)│
-╭───────┐Mount gocryptfs-Folder┌───────────────────────╯
+╭───────┐Mount gocryptfs-Folder┌──────────────────────────────╯
 │${n}\n"
 [ -s ${2}/.gocryptfs.reverse.conf ] || printf "${r}\n  ──────┤!!! Data-Folder NOT OK !!!├──────${n}\n"
 [ -s ${2}/.gocryptfs.reverse.conf ] || exit 1
@@ -70,14 +70,14 @@ mount |grep $3 || gocryptfs -passfile $1 -reverse $2 $3
 WAIT 5
 #printf "${g} $(tput hpa 55)┬\n$(tput hpa 55)│"
 printf "${g}│
-╰───────┐SYNC gocryptfs-Folder┌───────────────────────────╮
-╭─────────────────────────────────────────────────────────╯
+╰───────┐SYNC gocryptfs-Folder┌───────────────────────────────╮
+╭─────────────────────────────────────────────────────────────╯
 │${n}\n"
 rclone lsl ${4}/gocryptfs.conf || printf "${r}\n  ──────┤!!! rclone-Remote-Path NOT OK !!!├──────${n}\n"
 [ -s ${3}/gocryptfs.conf ] && rclone lsl ${4}/gocryptfs.conf && time rclone sync -vP $3 $4
 printf "${g}│
-╰───────┐CHECK SYNC┌───────────────────────────╮
-╭────────────────────────────────╯UMOUNT╰──────╯
+╰───────┐CHECK SYNC┌──────────────────────────────────────────╮
+╭───────────────────────────────────────────────╯UMOUNT╰──────╯
 │${n}\n"
 WAIT 10
 time rclone check -vP $3 $4
