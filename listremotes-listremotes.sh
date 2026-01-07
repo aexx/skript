f100f354380b3001ea67379bfee41e9c1ab7fbbc
aexx <aexx@users.noreply.github.com>
2026-01-07 16:04:58 +0100

    Create listremotes-listremotes.sh

---
+++ listremotes-listremotes.sh
@@ -0,0 +1,1 @@
+printf "\n\n";rclone listremotes|while read a; do printf "┌───────────────┐$a┌────────────────────────────────────────────╁";sleep 2;printf "\n│$(rclone about ${a})\n";printf "└─────────┐\n";rclone lsd $a;done;echo;lt .config/rclone
