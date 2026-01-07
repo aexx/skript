7080d131f80cab1e2d9211217565cb7c65a5dfd4
aexx <aexx@users.noreply.github.com>
2026-01-07 16:16:05 +0100

    Refactor listremotes-listremotes.sh for readability

--- listremotes-listremotes.sh
+++ listremotes-listremotes.sh
@@ -1,1 +1,15 @@
-printf "\n\n";rclone listremotes|while read a; do printf "┌───────────────┐$a┌────────────────────────────────────────────╁";sleep 2;printf "\n│$(rclone about ${a})\n";printf "└─────────┐\n";rclone lsd $a;done;echo;lt .config/rclone
+#!/bin/bash
+
+printf "\n\n"
+rclone listremotes|while read a
+ do 
+  printf "┌───────────────┐$a┌────────────────────────────────────────────╁"
+  sleep 2
+  printf "\n│$(rclone about ${a})\n"
+  printf "└─────────┐\n"
+  rclone lsd $a
+done
+echo
+lt .config/rclone
+
+
