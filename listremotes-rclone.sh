1bcbb7dd0119cf954c062b6e93ca81a724eb3582
aexx <aexx@users.noreply.github.com>
2026-01-07 16:43:15 +0100

    Update listremotes-rclone.sh

--- listremotes-rclone.sh
+++ listremotes-rclone.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+sc="$(tput setaf 0)"
+ro="$(tput setaf 1)"
+gr="$(tput setaf 2)"
+ge="$(tput setaf 3)"
+bl="$(tput setaf 4)"
+ma="$(tput setaf 5)"
+hb="$(tput setaf 6)"
+we="$(tput setaf 7)"
+fett=$(tput bold)
+norm=$(tput sgr0)
+
 printf "\n\n"
 rclone listremotes|while read a
  do 
-  printf "┌───────────────┐$a┌────────────────────────────────────────────╁"
+  printf "${gr}┌───────────────┐$a┌────────────────────────────────────────────╁"
   sleep 2
   printf "\n│$(rclone about ${a})\n"
   printf "└─────────┐\n"
