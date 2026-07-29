37f9a2f050930b6f879e13776996589c612619ea
aexx <aexx@users.noreply.github.com>
2026-07-29 14:42:23 +0200

    Enhance gocryptfs-rclone-restore-help.sh with colors
    Added color formatting and usage instructions to the script.

--- gocryptfs-rclone-restore-help.sh
+++ gocryptfs-rclone-restore-help.sh
@@ -1,1 +1,46 @@
+#!/bin/bash
+
+sc="$(tput setaf 0)"
+ro="$(tput setaf 1)"
+gr="$(tput setaf 2)"
+ge="$(tput setaf 3)"
+bl="$(tput setaf 4)"
+ma="$(tput setaf 5)"
+hb="$(tput setaf 6)"
+we="$(tput setaf 7)"
+fett=$(tput bold)
+bon=$(tput smso)  # set bold on
+boff=$(tput rmso) # remove bold
+norm=$(tput sgr0)
+
+
+usage ()
+{
+        printf "\n${g}
+╭────┐Usage┌────────────────────────────────────────────────────────────────────────╮
+│                                                                                   │
+│ $(basename $0) ${n}filename-to-find-for-restore REMOTE-PATH [LOCAL-PATH] ${g} $(tput hpa 83) │
+│                                                                                   │
+╰───────────────────────────────────────────────────────────────────────────────────╯ "
+}
+
+if [ $# -lt 2 ]
+ then
+  usage ; echo
+  exit 1
+ else
+  usage
+fi
+
+if [ -z $2 ] 
+   then
+    LOCALDIR=${HOME}/bigaex/ 
+   else
+    LOCALDIR=${2}
+  fi
+
+
+printf "\n\n"
+printf "${gr}┌───────────────┐$0┌────────────────────────────────────────────╁"
+
 
